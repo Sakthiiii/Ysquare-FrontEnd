6b9f18a39139cd68522a00e81f2f0328e9394418
Sakthiiii <sakthi.tamil313@gmail.com>
2023-08-17 13:49:29 +0530

    changes

---
+++ build/precache-manifest.8264a347a878ff557fa3b6cc9b1fb2ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd179c4da50330105bcf1d09a2656ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9a34c8c47ebc00f29c",
+    "url": "/static/css/main.d0aa0f32.chunk.css"
+  },
+  {
+    "revision": "97951b10fe9e6a2cc617",
+    "url": "/static/js/2.e6c49ee7.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.e6c49ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9a34c8c47ebc00f29c",
+    "url": "/static/js/main.69c522ae.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.69c522ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  }
+]);
